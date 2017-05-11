bba7b7db5ef768cd7b0b51a9aeeb647473d54aa6
august.conwell <august.conwell@gmail.com>
2017-05-11 09:12:00 -0400

    Finished

--- public/js/fe.js
+++ public/js/fe.js
@@ -1,6 +1,6 @@
 $(document).ready(function () { 
     $('#new-burger-button').click(function () { 
+        $('#new-burger-button').css('display', 'none');
         $('.modal').css('display', 'inline');
-        console.log('h')
     })
 })
